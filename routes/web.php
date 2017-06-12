031dcf5f531dcd4f415eb04ee2f71f6cc83b4b2e
lanse33zjj <lanse33zjj@163.com>
2017-06-12 15:55:27 +0800

    lar11111

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+//    return view('welcome');
+    return "<p style='margin-top: 22%;text-align: center'>PHP是世界上最好的语言<p>";
+});
